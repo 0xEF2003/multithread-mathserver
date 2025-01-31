e4691659d8f00deba176e4aaeae3d73ef55cde59
ekroll <ekroll@pm.me>
2025-01-31 10:57:33 +0100

    theoretical multithread fix

--- src/main/java/no/ntnu/stud/Main.java
+++ src/main/java/no/ntnu/stud/Main.java
@@ -4,7 +4,9 @@ package no.ntnu.stud;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-        Server server = new Server(1234);
-        server.run();
+        int port = 1234;
+        boolean multithreading = true;
+        Server server = new Server(port);
+        server.run(multithreading);
     }
 }
