ec888091d0b240828b905d696161e0e775afbd5f
Henrik <henaa@stud.ntnu.no>
2025-01-31 10:43:14 +0100

    Dumb dumb bad multithread

--- src/main/java/no/ntnu/stud/Server.java
+++ src/main/java/no/ntnu/stud/Server.java
@@ -20,40 +20,66 @@ public class Server {
    *
    * <p>Waits for clients and listens to commands.
    */
-  public void run() {
+  public void run(boolean multiThreaded) {
+    if (multiThreaded) {
+      runMultiThreaded();
+    } else {
+      runSingleThreaded();
+    }
+  }
+
+  private void runSingleThreaded() {
     try {
-      while (true){
+      while (true) {
         ServerSocket server = new ServerSocket(this.port);
         Socket client = server.accept();
+        handleClient(client);
         server.close();
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        String line;
-        while((line = in.readLine()) != null) {
-            Request request = null;
-            double result = 0;
-            String message = "";
-
-            // Parse request
-            try { request = new Request(line); }
-            catch (Exception e) { message = "Bad request"; }
-
-            try { // Do math operation
-                result = MathHandler.perform(request); 
-                message = String.valueOf(result);
-            }
-            catch (Exception e) {
-              message = "Bad math";
-              System.out.println(e.getMessage());
-            }
-
-            // Send result back to client
-            out.println(message);
-        }
       }
+    } catch (IOException e) {
+        System.out.println(e.getMessage());
     }
-    catch(IOException e) {
-      System.out.println(e);
+  }
+
+  private void runMultiThreaded(){
+   new Thread(() -> {
+     try {
+       ServerSocket server = new ServerSocket(this.port);
+       while (true) {
+         Socket client = server.accept();
+         handleClient(client);
+         server.close();
+       }
+     } catch (IOException e) {
+       System.out.println(e.getMessage());
+     }
+    }).start();
+  }
+
+  private void handleClient(Socket client) throws IOException {
+    PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    String line;
+    while((line = in.readLine()) != null) {
+      Request request = null;
+      double result = 0;
+      String message = "";
+
+      // Parse request
+      try { request = new Request(line); }
+      catch (Exception e) { message = "Bad request"; }
+
+      try { // Do math operation
+        result = MathHandler.perform(request);
+        message = String.valueOf(result);
+      }
+      catch (Exception e) {
+        message = "Bad math";
+        System.out.println(e.getMessage());
+      }
+
+      // Send result back to client
+      out.println(message);
     }
   }
 }
